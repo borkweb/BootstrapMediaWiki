32f10362ee481f6e39e470ea482810b1a2b4f5a2
Rob Myers <rob@robmyers.org>
2015-06-26 16:40:17 -0700

    Modify the theme to not show a login form by implementing an extension (add this to LocalSettings.php to enable, see code).

---
+++ NoUserLoginForm.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Remove the standard user login form
+ *
+ * require_once($IP."/skins/bootstrap-mediawiki/NoUserLoginForm.php");
+ *
+ * @author Creative Commons Corporation
+ * @license GPLv2+
+ * @version 0.1
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+    die( 'This file is a MediaWiki extension and not a valid entry point' );
+}
+
+$wgExtensionCredits['other'][] = array(
+    'name'        => 'NoUserCreateForm',
+    'version'     => '0.1',
+    'author'      => 'Creative Commons Corporation',
+    'url'         => 'https://github.com/creativecommons/bootstrap-mediawiki',
+    'description' => 'Removes the standard user creation form'
+);
+
+$wgHooks['UserLoginForm'][] = 'noUserLoginForm';
+
+class NoUserLoginFormTemplate extends BaseTemplate {
+
+	function execute() {
+        // A link to log in/out would be confusing, text would require i18n
+        echo '<span style="font-size:24pt">&#10004;</span>';
+    }
+}
+
+function noUserLoginForm( &$template ) {
+    $template = new NoUserLoginFormTemplate();
+	return true ;
+}
