073097ae05932ac70614774544918bbf6b259fed
Matthew Batchelder <borkweb@gmail.com>
2025-07-10 14:12:15 -0400

    Convert from grunt to webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+	entry: {
+		// Main stylesheet entry point
+		style: './resources/main.less'
+	},
+	output: {
+		path: path.resolve(__dirname),
+		filename: '[name].js', // We'll delete the JS files since we only need CSS
+	},
+	module: {
+		rules: [
+			{
+				test: /\.less$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'less-loader',
+						options: {
+							lessOptions: {
+								paths: [path.resolve(__dirname, 'resources')],
+							},
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+	],
+	optimization: {
+		minimizer: [
+			`...`, // Keep default minimizers
+			new CssMinimizerPlugin(),
+		],
+	},
+	// Since we're only building CSS, we can ignore the JS output
+	externals: {
+		// Prevent webpack from bundling these
+		jquery: 'jQuery',
+	},
+};
